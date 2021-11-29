7664fcd367884f33436ba149d6eef9de14c9ae62
Ubuntu <hussain@dev-jenkins.ofuogsxvp4relmbb2hx3llgxsc.cwx.internal.cloudapp.net>
2021-11-29 18:03:43 +0000

    adding to dev branch

---
+++ backend/application/models.py
@@ -0,0 +1,14 @@
+from application import db
+
+
+class Players(db.Model):
+    player_id = db.Column(db.Integer, primary_key=True)
+    player_name = db.Column(db.String(30), nullable=False)
+    player_position= db.Column(db.String(3), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.team_id"), nullable=False)
+    #teams = db.relationship('Teams', backref='player')
+
+class Teams(db.Model):
+    team_id = db.Column(db.Integer, primary_key=True)
+    team_name = db.Column(db.String(30), nullable=False)
+    players = db.relationship('Players', backref='team')
